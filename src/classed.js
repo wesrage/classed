bb262b9523981b573059a22ea2e937a9740c37ef
Wesley Waldner <wesley.waldner@yardi.com>
2016-11-10 14:36:54 -0600

    Add displayName

--- src/classed.js
+++ src/classed.js
@@ -2,9 +2,13 @@ import React from 'react';
 import classnames from 'classnames';
 
 const classed = {};
-const factory = tagName => classes => props => {
-    const className = classnames(classes, props.className);
-    return React.createElement(tagName, { ...props, className });
+const factory = tagName => classes => {
+    const component = props => {
+        const className = classnames(classes, props.className);
+        return React.createElement(tagName, { ...props, className });
+    };
+    component.displayName = `classed.${tagName}(${classes})`;
+    return component;
 };
 
 Object.keys(React.DOM).forEach(tagName => {
